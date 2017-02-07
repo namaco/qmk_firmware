60d6ab21c49d1cfbf7bac54e2f52c92d5d50fcea
unknown <>
2017-02-07 10:09:05 +0900

    Mod Func key

--- keyboards/ergodox/keymaps/namaco/keymap.c
+++ keyboards/ergodox/keymaps/namaco/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | LGui   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '"   |
  * |--------+------+------+------+------+------| MACFN|           | MACFN|------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ * `--------+------+------+------+------+-------------'            `-------------+------+------+------+------+--------'
  *   |  cut | copy | paste| Left | Right|                                       |  Up  | Down |   [  |   ]  | LWin  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,         KC_1,         KC_2,        KC_3,      KC_4,   KC_5,   KC_EQL,
         KC_TAB,         KC_Q,         KC_W,        KC_E,      KC_R,   KC_T,   KC_LBRC,
         KC_LGUI,        KC_A,         KC_S,        KC_D,      KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,        KC_C,      KC_V,   KC_B,   LT(MACFN, LGUI(KC_SPC)),
+        KC_LSFT,        KC_Z,         KC_X,        KC_C,      KC_V,   KC_B,   MO(MACFN),
         LGUI(KC_X),     LGUI(KC_C),   LGUI(KC_V),  KC_LEFT,   KC_RGHT,
                                                                       KC_LCTL,KC_LALT,
                                                                               KC_HOME,
@@ -62,9 +62,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |   [  |           |   ]  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|   {  |           |   }  |------+------+------+------+------+--------|
  * | CTRL   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '"   |
- * |--------+------+------+------+------+------| LGui+|           | LFn  |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  | Space|           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ * |--------+------+------+------+------+------| WINFn|           | WINFn|------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * `--------+------+------+------+------+-------------'            `-------------+------+------+------+------+--------'
  *   |  cut | copy | paste| Left | Right|                                       |  Up  | Down |   [  |   ]  | LMac |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,         KC_1,         KC_2,         KC_3,       KC_4,   KC_5,   KC_EQL,
         KC_TAB,         KC_Q,         KC_W,         KC_E,       KC_R,   KC_T,   KC_LBRC,
         KC_LCTL,        KC_A,         KC_S,         KC_D,       KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,         KC_C,       KC_V,   KC_B,   LT(WINFN, LALT(KC_GRV)),
+        KC_LSFT,        KC_Z,         KC_X,         KC_C,       KC_V,   KC_B,   MO(WINFN),
         LCTL(KC_X),     LCTL(KC_C),   LCTL(KC_V),   KC_LEFT,    KC_RGHT,
                                                                         KC_LGUI,    KC_LALT,
                                                                                     KC_HOME,
@@ -150,10 +150,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  | F6   |           | F7   |  F8  |  F9  | F10  |  F11 |  F12 | Alt+`  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
- * |--------+------+------+------+------+------| PRSC |           |      |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| PRSC |           |A+PRSC|------+------+------+------+------+--------|
  * |  LGui  |      |      |      |      |      |------|           |------|      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |W+PRSC|A+PRSC|           |      |      |      |      |      |      |        |
+ * |        |      |      |      |      |W+PRSC|      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      | W+← | W+↓ | W+↑ | W+→ |                                       |      |      |      |      |LMouse|
  *   `----------------------------------'                                       `----------------------------------'
@@ -171,7 +171,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Fnctions
 [WINFN] = KEYMAP(
        // left hand
-       KC_TRNS,       KC_F1,         KC_F2,       KC_F3,               KC_F4,                KC_F5,         KC_F6,
+       LALT(KC_GRV),  KC_F1,         KC_F2,       KC_F3,               KC_F4,                KC_F5,         KC_F6,
        KC_TRNS,       KC_TRNS,       KC_TRNS,     KC_TRNS,             KC_TRNS,              KC_TRNS,       KC_PSCR,
        KC_LGUI,       LCTL(KC_A),    LCTL(KC_S),  LCTL(KC_D),          LCTL(KC_F),           LCTL(KC_G),
        KC_TRNS,       LCTL(KC_Z),    LCTL(KC_X),  LCTL(KC_C),          LCTL(KC_V),           LGUI(KC_PSCR), KC_TRNS,
