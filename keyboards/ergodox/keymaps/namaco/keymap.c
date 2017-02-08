f604f59d91ebbe1d0ee538cc2adaa1dd7bd80673
unknown <>
2017-02-08 10:49:02 +0900

    Add IME switch key

--- keyboards/ergodox/keymaps/namaco/keymap.c
+++ keyboards/ergodox/keymaps/namaco/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_RBRC,    KC_Y,       KC_U,       KC_I,       KC_O,        KC_P,       KC_BSLS,
                     KC_H,       KC_J,       KC_K,       KC_L,        KC_SCLN,    KC_QUOT,
         MO(MACFN),  KC_N,       KC_M,       KC_COMM,    KC_DOT,      KC_SLSH,    KC_RSFT,
-                                KC_UP,      KC_DOWN,    KC_LBRC,     KC_RBRC,    TO_WIN,
+                                KC_UP,      KC_DOWN,    KC_LBRC,     GUI_SP,     TO_WIN,
         KC_LALT,    TO_MOUSE,
         KC_PGUP,
         KC_PGDN,    KC_DELT,    KC_ENT
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_RBRC,    KC_Y,       KC_U,       KC_I,       KC_O,        KC_P,       KC_BSLS,
                     KC_H,       KC_J,       KC_K,       KC_L,        KC_SCLN,    KC_QUOT,
         MO(WINFN),  KC_N,       KC_M,       KC_COMM,    KC_DOT,      KC_SLSH,    KC_RSFT,
-                                KC_UP,      KC_DOWN,    KC_LBRC,     KC_RBRC,    TO_MAC,
+                                KC_UP,      KC_DOWN,    KC_LBRC,     ALT_GRV,    TO_MAC,
         KC_LALT,    TO_MOUSE,
         KC_PGUP,
         KC_PGDN,    KC_DELT,    KC_ENT
